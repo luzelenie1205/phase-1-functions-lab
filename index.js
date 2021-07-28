128839194173b5525849e488e618f02a028b0c4d
luzelenie <luzelenie1205@gmail.com>
2021-07-27 21:49:11 -0400

    commit tree e72c9c67b30506e875f21f5fc7d20aa51050a827

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block1){  
+    return Math.abs(block1-42);
+}
+ function distanceFromHqInFeet(block){
+     return distanceFromHqInBlocks(block)*264
+ }
+ function  distanceTravelledInFeet(block1,block2){
+return Math.abs(block1-block2)*264;
+ }
+
+ function calculatesFarePrice(start,destination){
+     const distance = distanceTravelledInFeet(start,destination)
+    
+    if (distance <= 400 ){
+        return 0;
+    }
+    
+else if (distance > 400 && distance<= 2000 ) {
+        return ((distance -400) * .02);
+}
+else if (distance > 2000 && 2500>=distance){
+    return 25;
+}
+else{
+    return 'cannot travel that far';
+}
+
+}
